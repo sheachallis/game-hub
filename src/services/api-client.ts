5f6fc18b19ebedf5ffb2b330574d9263ec165abf
Shea Challis <shea.challis@hotmail.co.uk>
2023-09-16 11:35:54 +0100

    Building game cards

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "1ae7d3efce13495596d1fe16d91b836b",
+    key: "3cd1e709853c4046a59fd8dcb23aacea",
   },
 });
